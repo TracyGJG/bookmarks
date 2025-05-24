f8c1332757a337b8360210a3a2b306b3cde93755
Tracy-Gregory Gilmore <tracyg.gilmore@gmail.com>
2025-05-24 20:25:36 +0100

    Store and retrieve selected tab

--- bookmarks.js
+++ bookmarks.js
@@ -1,7 +1,13 @@
-document.querySelector("main").addEventListener("click", (evt) => {
-  if (evt.target.tagName === "SUMMARY") {
-    [...document.querySelectorAll("details")].forEach((detail) =>
-      detail.removeAttribute("open")
+document.querySelector('main').addEventListener('click', (evt) => {
+  if (evt.target.tagName === 'SUMMARY') {
+    window.localStorage.setItem('selectedDet', evt.target.parentNode.id);
+    [...document.querySelectorAll('details')].forEach((detail) =>
+      detail.removeAttribute('open')
     );
   }
+});
+
+window.addEventListener('load', () => {
+  const selectedDet = window.localStorage.getItem('selectedDet') ?? 'detHome';
+  document.getElementById(selectedDet).setAttribute('open', 'true');
 });
